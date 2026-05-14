6807c32c21fb2aa1a06c1d30400d4a7412f9a2dd
xaskilaki008 <fff007fqp@gmail.com>
2026-05-14 16:32:23 +0300

    запуск таймера на час

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule; // Для работы планировщика
+use Illuminate\Support\Facades\Cache;    // Для проверки кнопки Вкл/Выкл
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// --- НАШ ПЛАНИРОВЩИК (Шаг 2) ---
+Schedule::command('wave:fetch')
+    ->hourly() // Запуск каждый час
+    ->when(function () {
+        // Команда выполнится ТОЛЬКО если в кэше статус = true (то есть парсер включен)
+        return Cache::get('parsing_enabled', true);
+    });
