3fb8e0ef42cef37b423696db3352b09cc8594344
xaskilaki008 <fff007fqp@gmail.com>
2026-05-28 11:54:41 +0300

    добавил автообновление страницы каждые 10 секунд, пока DWD-загрузка находится в состоянии running

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Панель администратора</title>
+    @if(($fetchStatus['running'] ?? false) && !($fetchStatus['stale'] ?? false))
+        <meta http-equiv="refresh" content="10">
+    @endif
     @vite(['resources/css/app.css'])
 </head>
 <body class="admin-page">
